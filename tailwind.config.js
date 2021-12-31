616cb9b4bba0f81201cbfb1e582df3bb38a7b854
Reinhart Andreas <reinhartandreas96@gmail.com>
2021-12-31 15:04:28 +0700

    chore: tailwind colors setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#evebff',
+      'bubble-gum': '#ff77e9',
+      'grey': '#E8E8E8',
+      'red': '#ED5858',
+      'dark': '#262B31',
+    },
   },
   variants: {
     extend: {},
