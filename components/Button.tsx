0d37c4420de15aaff5aa0b1a3668cba4de5989d7
Reinhart Andreas <reinhartandreas96@gmail.com>
2021-12-31 23:06:28 +0700

    fix: interface of ButtonProps, extra className prop support

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { ButtonHTMLAttributes } from 'react';
 
-interface ButtonProps {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   href?: string;
+  className?: string;
   children: React.ReactNode;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, href, ...params }) => {
-  const className =
-    'inline-block px-5 py-3 font-medium leading-snug border border-transparent text-base rounded-md text-white bg-red hover:bg-midnight focus:outline-none focus:shadow-outline transition duration-150 ease-in-out';
+const Button: React.FC<ButtonProps> = ({
+  children,
+  href,
+  className = '',
+  ...params
+}) => {
+  const classes = `inline-block px-5 py-3 font-medium leading-snug border border-transparent text-base rounded-md text-white bg-red hover:bg-midnight focus:outline-none focus:shadow-outline transition duration-150 ease-in-out ${className}`;
 
   if (href) {
     return (
@@ -15,14 +20,14 @@ const Button: React.FC<ButtonProps> = ({ children, href, ...params }) => {
         href={href}
         target="_blank"
         rel="noopener noreferrer"
-        className={className}
+        className={classes}
       >
         {children}
       </a>
     );
   } else {
     return (
-      <button className={className} {...params}>
+      <button className={classes} {...params}>
         {children}
       </button>
     );
