78f26f30ec41aad73b8ce15e6b11e5b8c0da74ef
Reinhart Andreas <reinhartandreas96@gmail.com>
2022-01-11 23:35:43 +0700

    refactor: add LayoutProps

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
 
-const Layout = ({ children }) => {
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
       <Header />
