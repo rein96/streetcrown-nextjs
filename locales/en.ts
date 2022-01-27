85f7aab7802359cdd684778cb98d36afe6552722
Reinhart Andreas <reinhartandreas96@gmail.com>
2022-01-27 21:31:34 +0700

    feat: add en and id locales

---
+++ locales/en.ts
@@ -0,0 +1,20 @@
+export default {
+  trained_detailer: 'Trained Detailers',
+  trainer_detailer_description:
+    'Our details have extensive experience and have passed training from the trusted detailers',
+  premium_product: 'Premium Class Product',
+  premium_product_description:
+    'We only use the best premium detailing products that we have researched',
+  experienced_detailer: 'Experienced Detailers',
+  experienced_detailer_description:
+    'We have extensive detailing experiences to all vehicles, including, motorcycles, new cars, vintage cars, bicycles',
+  warranty: 'Warranty',
+  warranty_description:
+    'The coating package has a 2-year warranty *available for Nano Ceramic Coating, Diamond Ceramic Coating',
+  pickup_delivery: 'Pick Up & Delivery Available',
+  pickup_delivery_description:
+    'We can help you to pick up and deliver your vehicle',
+  standardized_procedure: 'Standardized Procedure',
+  standardized_procedure_description:
+    'We apply our Standard Operating Procedure to make sure we provide the best and most consistent results to all our customers',
+};
---
+++ locales/id.ts
@@ -0,0 +1,20 @@
+export default {
+  trained_detailer: 'Detailer Terlatih',
+  trainer_detailer_description:
+    'Detailer kami memiliki pengalaman dan telah melewati training oleh pelatih detailing terpercaya',
+  premium_product: 'Produk kelas premium',
+  premium_product_description:
+    'Kami hanya menggunakan produk detailing premium yang terbaik yang telah kami research',
+  experienced_detailer: 'Detailer berpengalaman',
+  experienced_detailer_description:
+    'Kami memiliki pengalaman detaiing ke semua jenis kendaraan, sepeda motor, mobil baru, mobil tua, dan sepeda.',
+  warranty: 'Warranty',
+  warranty_description:
+    'Paket coating memiliki warranty selama 2 tahun *berlaku untuk Nano Ceramic Coating, Diamond Ceramic Coating',
+  pickup_delivery: 'Tersedia Pick up & Delivery',
+  pickup_delivery_description:
+    'Kami siap mengantar dan menjemput kendaraan anda',
+  standardized_procedure: 'Prosedur terstandardisasi',
+  standardized_procedure_description:
+    'Kami menerapkan Standard Operating Procedure yang ketat untuk memastikan hasil maksimal dan konsisten untuk semua customer',
+};
