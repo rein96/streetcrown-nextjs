3b80f520e3e9ed611e3abf9d89fa837f47aeb332
Reinhart Andreas <reinhartandreas96@gmail.com>
2022-01-30 20:29:46 +0700

    feat: add configuration for google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,28 @@
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { pageview } from 'lib/gtag';
 import 'styles/globals.scss';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  // Setup Google Analytics
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      // Invoke analytics function only for production mode
+      if (isProduction) pageview(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
