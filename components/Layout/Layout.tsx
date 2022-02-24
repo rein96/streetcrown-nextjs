cbe89d82aa710b46e568e683ec54bd4c28dcf829
Reinhart Andreas <reinhartandreas96@gmail.com>
2022-02-24 22:22:43 +0700

    perf: reduce re-rendering when scrolling by implementing debounce

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -6,32 +6,48 @@ interface LayoutProps {
   children: React.ReactNode;
 }
 
+function debounce(func: Function, wait: number, immediate?: boolean) {
+  let timeout;
+  return function () {
+    // const context = this;
+    const context = window;
+    // const args = arguments;
+    const later = function () {
+      timeout = null;
+      // if (!immediate) func.apply(context, args);
+      if (!immediate) func.apply(context);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    // if (callNow) func.apply(context, args);
+    if (callNow) func.apply(context);
+  };
+}
+
 /** Reusable Layout component for pages */
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   const [y, setY] = useState(0);
 
   const [scrollDirection, setScrollDirection] = useState<'up' | 'down'>('up');
 
-  const handleNavigation = useCallback(
-    (e) => {
-      if (y > window.scrollY) {
-        // Scrolling up
-        setScrollDirection('up');
-      } else if (y < window.scrollY) {
-        // Scrolling down
-        setScrollDirection('down');
-      }
-      setY(window.scrollY);
-    },
-    [y]
-  );
+  const handleNavigation = debounce(() => {
+    if (y > window.scrollY) {
+      // Scrolling up
+      setScrollDirection('up');
+    } else if (y < window.scrollY) {
+      // Scrolling down
+      setScrollDirection('down');
+    }
+    setY(window.scrollY);
+  }, 100);
 
   useEffect(() => {
     window.addEventListener('scroll', handleNavigation);
     return () => {
       window.removeEventListener('scroll', handleNavigation);
     };
-  }, [handleNavigation]);
+  }, [handleNavigation, y, scrollDirection]);
 
   return (
     <>
