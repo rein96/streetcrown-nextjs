5463d29a51625407062aa18a642be3628285eead
Reinhart Andreas <reinhartandreas96@gmail.com>
2022-10-02 20:03:50 +0700

    feat(api): revalidate-detailing api

---
+++ pages/api/revalidate-detailing.ts
@@ -0,0 +1,46 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+interface ResponseData {
+  revalidated?: boolean;
+  error?: string;
+  message?: string;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  // check for the POST request
+  if (req.method !== 'POST') {
+    return res
+      .status(400)
+      .json({ error: 'Invalid HTTP method. Only POST requests are allowed.' });
+  }
+
+  // check for the secret token
+  if (req.query.secret !== process.env.REVALIDATE_SECRET_TOKEN) {
+    return res.status(401).json({
+      message: 'Invalid token',
+    });
+  }
+
+  try {
+    // check that body is not empty
+    const body = req.body;
+    if (!body) {
+      res.status(400).send({ error: 'Bad request (no body)' });
+      return;
+    }
+
+    // get the slug to revalidate from body
+    const slugToRevalidate = body.slugToRevalidate;
+    if (slugToRevalidate) {
+      await res.revalidate(`/detailing/${slugToRevalidate}`);
+      return res.json({ revalidated: true });
+    }
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send({ error: 'Error revalidating' });
+  }
+}
